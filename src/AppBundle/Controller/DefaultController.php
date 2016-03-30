209336e4a4f3f5f77645862708f647a51b850cde
Andrey Kravchuk <taptak1989@gmail.com>
2016-03-30 11:38:59 +0300

    add recently_reviewed

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -39,31 +39,7 @@ class DefaultController extends Controller
      */
     public function testAction(Request $request)
     {
-//        return $this->redirectToRoute('homepage');
-
-//        $qweqwe = $this->get('cron_mailer')->FollowThePrice();
-//        $data = $this->getDoctrine()->getRepository('AppBundle:Orders')->findOneBy(array(
-//            'phone'=> '+38(097)-152-38-54',
-//            'type' => 'Обычный заказ'
-//        ));
-        $em = $this->getDoctrine()->getManager()->getConnection();
-        $statement = $em->prepare("SELECT id FROM product_models WHERE active = 1 AND published = 1 ORDER BY RAND() LIMIT 12");
-        $statement->execute();
-        $results = $statement->fetchAll();
-        $results = array_column ($results, 'id');
-
-        $data = $this->getDoctrine()->getRepository('AppBundle:ProductModels')->findBy(
-            array('active'=>1,'published'=>1,'id'=>$results),
-            array('id'=>'ASC')
-        );
-        return $this->render('AppBundle:userpart:home.html.twig', array(
-            'base_dir'          => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-            'params'            => $this->get('options')->getParams(),
-            'cart'              => $this->get('cart')->getHeaderBasketInfo(),
-            'compare'           => $this->get('compare')->getHeaderCompareInfo(),
-            'popular'           => $data,
-            'recently_reviewed' => $this->get('entities')->getRecentlyViewed(),
-        ));
+        return $this->redirectToRoute('homepage');
     }
 
 }
