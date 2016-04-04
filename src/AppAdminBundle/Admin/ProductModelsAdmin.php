e91b253341e53f22d2ac897d69e66ab074a1f311
Zimm <Romsssa@gmail.com>
2016-04-04 11:43:24 +0300

    Remove model images from product

--- src/AppAdminBundle/Admin/ProductModelsAdmin.php
+++ src/AppAdminBundle/Admin/ProductModelsAdmin.php
@@ -29,8 +29,7 @@ class ProductModelsAdmin extends Admin
             ->add('inStock', null, array('label' => 'Наличие на складе'))
             ->add('published', null, array('label' => 'Опубликовано'))
             ->add('createTime', null, array('label' => 'Дата создания'))
-            ->add('updateTime', null, array('label' => 'Дата последнего изменения'))
-        ;
+            ->add('updateTime', null, array('label' => 'Дата последнего изменения'));
     }
 
     /**
@@ -59,8 +58,7 @@ class ProductModelsAdmin extends Admin
                     'edit' => array(),
                     'delete' => array(),
                 )
-            ))
-        ;
+            ));
     }
 
     /**
@@ -70,89 +68,74 @@ class ProductModelsAdmin extends Admin
     {
         $formMapper
             ->tab('Модель')
-                ->with('ProductModels',
+            ->with('ProductModels',
+                array(
+                    'class' => 'col-md-12',
+                ))
+            ->add('name', null, array('label' => 'Название модели'))
+            ->add('content', null, array('label' => 'О модели', 'attr' => array('class' => 'ckeditor')))
+            ->add('characteristics', null, array('label' => 'Характеристики', 'attr' => array('class' => 'ckeditor')))
+            ->add('features', null, array('label' => 'Особенности', 'attr' => array('class' => 'ckeditor')))
+            ->add('alias', null, array('label' => 'Ссылка'))
+            ->add('productColors', 'entity',
+                array(
+                    'class' => 'AppBundle:ProductColors',
+                    'property' => 'name',
+                    'label' => 'Цвет модели',
+                    'empty_value' => 'Выберите цвет модели'
+                )
+            )
+            ->add('sizes', 'entity',
+                array(
+                    'class' => 'AppBundle:ProductModelSizes',
+                    'multiple' => true,
+                    'property' => 'size',
+                    'label' => 'Размер',
+                    'empty_value' => 'Выберите размер модели'
+                )
+            )
+            ->add('decorationColor', 'entity',
+                array(
+                    'class' => 'AppBundle:ProductColors',
+                    'property' => 'name',
+                    'label' => 'Цвет отделки',
+                    'empty_value' => 'Выберите цвет отделки',
+                    'required' => false
+                )
+            )
+            ->add('price', null, array('label' => 'Цена'))
+            ->add('oldprice', null, array('label' => 'Старая цена'))
+            ->add('seoTitle', null, array('label' => 'СЕО заглавие'))
+            ->add('seoDescription', null, array('label' => 'СЕО описание'))
+            ->add('seoKeywords', null, array('label' => 'СЕО кейворды'))
+            ->add('priority', null, array('label' => 'Приоритет'))
+            ->add('active', null, array('label' => 'Активная'))
+            ->add('inStock', null, array('label' => 'Наличие на складе'))
+            ->add('published', null, array('label' => 'Опубликовано'))
+            ->end()
+            ->end();
+
+        if (!$this->hasParentFieldDescription()) { // this Admin is embedded
+            $formMapper->tab('Изображения модели')
+                ->with('ProductModelImages',
                     array(
-                        'class'       => 'col-md-12',
-                    ))
-                    ->add('name', null, array('label' => 'Название модели'))
-                    ->add('content', null, array('label' => 'О модели','attr' => array('class' => 'ckeditor')))
-                    ->add('characteristics', null, array('label' => 'Характеристики','attr' => array('class' => 'ckeditor')))
-                    ->add('features', null, array('label' => 'Особенности','attr' => array('class' => 'ckeditor')))
-                    ->add('alias', null, array('label' => 'Ссылка'))
-                    ->add('productColors', 'entity',
-                        array(
-                            'class'         => 'AppBundle:ProductColors',
-                            'property'      => 'name',
-                            'label'         => 'Цвет модели',
-                            'empty_value'   => 'Выберите цвет модели'
-                        )
+                        'class' => 'col-md-12',
                     )
-                    ->add('sizes', 'entity',
-                        array(
-                            'class'         => 'AppBundle:ProductModelSizes',
-                            'multiple'      => true,
-                            'property'      => 'size',
-                            'label'         => 'Размер',
-                            'empty_value'   => 'Выберите размер модели'
-                        )
+                )
+                ->add('productModelImages', 'sonata_type_collection',
+                    array(
+                        //'class'    => 'AppBundle:ProductModelImages',
+                        'label' => 'Изображения',
+                    ),
+                    array(
+                        'edit' => 'inline',
+                        //'inline' => 'table',
+                        //'sortable'  => 'priority'
                     )
-                    ->add('decorationColor', 'entity',
-                        array(
-                            'class'         => 'AppBundle:ProductColors',
-                            'property'      => 'name',
-                            'label'         => 'Цвет отделки',
-                            'empty_value'   => 'Выберите цвет отделки',
-                            'required' => false
-                        )
-                    )
-                    ->add('price', null, array('label' => 'Цена'))
-                    ->add('oldprice', null, array('label' => 'Старая цена'))
-                    ->add('seoTitle', null, array('label' => 'СЕО заглавие'))
-                    ->add('seoDescription', null, array('label' => 'СЕО описание'))
-                    ->add('seoKeywords', null, array('label' => 'СЕО кейворды'))
-                    ->add('priority', null, array('label' => 'Приоритет'))
-                    ->add('active', null, array('label' => 'Активная'))
-                    ->add('inStock', null, array('label' => 'Наличие на складе'))
-                    ->add('published', null, array('label' => 'Опубликовано'))
+                )
                 ->end()
-            ->end()
-            ->tab('Изображения модели')
-            ->with('ProductModelImages',
-                array(
-                    'class'       => 'col-md-12',
-                )
-            )
-            ->add('productModelImages', 'sonata_type_collection',
-                array(
-                    //'class'    => 'AppBundle:ProductModelImages',
-                    'label' => 'Изображения',
-                ),
-                array(
-                    'edit' => 'inline',
-                    //'inline' => 'table',
-                    //'sortable'  => 'priority'
-                )
-            )
-            ->end()
-            ->end()
-            //->tab('Артикулы')
-                //->with('SkuProducts',
-                    //array(
-                        //'class'       => 'col-md-12',
-                    //))
-                    //->add('skuProducts', 'sonata_type_collection',
-                        //array(
-                            //'label' => 'Артикулы',
-                            //'by_reference' => false
-                        //),
-                        //array(
-                            //'edit' => 'inline',
-                            //'inline' => 'table',
-                        //)
-                    //)
-                //->end()
-            //->end()
-        ;
+                ->end();
+        }
     }
 
     /**
@@ -174,13 +157,12 @@ class ProductModelsAdmin extends Admin
             ->add('inStock', null, array('label' => 'Наличие на складе'))
             ->add('published', null, array('label' => 'Опубликовано'))
             ->add('createTime', null, array('label' => 'Дата создания'))
-            ->add('updateTime', null, array('label' => 'Дата последнего изменения'))
-        ;
+            ->add('updateTime', null, array('label' => 'Дата последнего изменения'));
     }
 
     public function preUpdate($param)
     {
-        foreach($param->getProductModelImages() as $image) {
+        foreach ($param->getProductModelImages() as $image) {
             $image->setProductModels($param);
         }
     }
