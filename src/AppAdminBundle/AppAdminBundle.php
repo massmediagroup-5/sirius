74e89e723f8e659fbf83b605c7dba64e4a4a1231
Zimm <Romsssa@gmail.com>
2016-04-11 10:09:11 +0300

    Admin roles

--- src/AppAdminBundle/AppAdminBundle.php
+++ src/AppAdminBundle/AppAdminBundle.php
@@ -2,8 +2,35 @@
 
 namespace AppAdminBundle;
 
+use Sonata\CoreBundle\Form\FormHelper;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppAdminBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $this->registerFormMapping();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $this->registerFormMapping();
+    }
+
+    /**
+     * Register form mapping information
+     */
+    public function registerFormMapping()
+    {
+        FormHelper::registerFormTypeMapping([
+            'sonata_security_roles' => 'AppAdminBundle\Form\Type\SecurityRolesType'
+        ]);
+    }
 }
