5ef0ffff9639f5a70f29e27643bdfc6969a1e805
Andrey Kravchuk <taptak1989@gmail.com>
2016-04-22 17:56:37 +0300

    search 0.1b

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -57,35 +57,45 @@ class SearchController extends Controller
      */
     public function searchAction($search)
     {
-        $slug = base64_decode(urldecode($search));
-        $finder = $this->get('fos_elastica.finder.app.products');
+        $slug = urldecode($search);
+        $finder = $this->get('fos_elastica.finder.app');
 
         $boolQuery = new \Elastica\Query\BoolQuery();
 
-        $fieldQuery = new \Elastica\Query\Match();
-        $fieldQuery->setFieldQuery('title', 'I am a title string');
-        $fieldQuery->setFieldParam('title', 'analyzer', 'my_analyzer');
-        $boolQuery->addShould($fieldQuery);
+        $productQuery = new \Elastica\Query\Match();
+        $productQuery->setFieldQuery('products.name', $slug);
+        $productQuery->setFieldParam('products.name', 'boost', 3);
+        $productQuery->setFieldParam('products.name', 'type', 'phrase_prefix');
+        $boolQuery->addShould($productQuery);
 
-        $tagsQuery = new \Elastica\Query\Terms();
-        $tagsQuery->setTerms('tags', array('tag1', 'tag2'));
-        $boolQuery->addShould($tagsQuery);
+        $articleQuery = new \Elastica\Query\Match();
+        $articleQuery->setFieldQuery('products.article', $slug);
+        $articleQuery->setFieldParam('products.article', 'boost', 3);
+        $articleQuery->setFieldParam('products.article', 'type', 'phrase_prefix');
+        $boolQuery->addShould($articleQuery);
 
-        $categoryQuery = new \Elastica\Query\Terms();
-        $categoryQuery->setTerms('categoryIds', array('1', '2', '3'));
-        $boolQuery->addMust($categoryQuery);
+        $baseCategoryQuery = new \Elastica\Query\Match();
+        $baseCategoryQuery->setFieldQuery('products.baseCategory.name', $slug);
+        $baseCategoryQuery->setFieldParam('products.baseCategory.name', 'boost', 3);
+        $baseCategoryQuery->setFieldParam('products.baseCategory.name', 'type', 'phrase_prefix');
+        $boolQuery->addShould($baseCategoryQuery);
 
+        $boolFilter = new \Elastica\Filter\Bool();
 
-        $result = $finder->find($slug);
-//        $result = $this->get('search')->doSearch($slug);
-//        var_dump($result);exit;
+        $boolFilter->addMust(
+            new \Elastica\Filter\Terms('active', array(1))
+        );
+
+        $boolFilter->addMust(
+            new \Elastica\Filter\Terms('productModels.published', array(1))
+        );
+        $filtered = new \Elastica\Query\Filtered($boolQuery, $boolFilter);
+        $query = \Elastica\Query::create($filtered);
+        $result = $finder->find($query,9999,array());
+        dump($result);exit;
         return $this->render('AppBundle:Search:search.html.twig', array(
             'data'              => $result,
-            'base_dir'          => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-            'params'            => $this->get('options')->getParams(),
-            'cart'              => $this->get('cart')->getHeaderBasketInfo(),
-            'compare'           => $this->get('compare')->getHeaderCompareInfo(),
-            'recently_reviewed' => $this->get('entities')->getRecentlyViewed(),
+            'base_dir'          => realpath($this->container->getParameter('kernel.root_dir').'/..')
         ));
     }
 
