2272c2a35079fc99faad4386dff7f8e27bcfc3e6
Zimm <Romsssa@gmail.com>
2016-04-22 17:40:20 +0300

    Fix orders page in profile

--- src/UserBundle/Controller/SecurityController.php
+++ src/UserBundle/Controller/SecurityController.php
@@ -24,29 +24,11 @@ class SecurityController extends BaseController
         /* @var $session \Symfony\Component\HttpFoundation\Session\Session */
 
         // get the error if any (works with forward and redirect -- see below)
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+        if (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
             $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
-            $error = '';
         }
 
-        if ($error) {
-            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
-            $error = $error->getMessage();
-        }
-        // last username entered by the user
-        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
-
-        $csrfToken = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
-
-        return $this->renderLogin(array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-            'csrf_token' => $csrfToken,
-        ));
+        return new RedirectResponse($this->container->get('router')->generate('homepage'));
     }
 
 }
