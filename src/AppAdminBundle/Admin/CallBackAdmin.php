b4a9094de37eaf7bf23916fb9bcf8a0688e9e5be
Andrey Kravchuk <taptak1989@gmail.com>
2016-06-02 17:01:50 +0300

    fixes 02-06-2016 v2

--- src/AppAdminBundle/Admin/CallBackAdmin.php
+++ src/AppAdminBundle/Admin/CallBackAdmin.php
@@ -10,14 +10,6 @@ use Sonata\AdminBundle\Show\ShowMapper;
 
 class CallBackAdmin extends Admin
 {
-    protected $datagridValues = [
-        '_page' => 1,            // display the first page (default = 1)
-        '_sort_order' => 'DESC', // reverse order (default = 'ASC')
-        '_sort_by' => 'createTime'  // name of the ordered field
-
-        // the '_sort_by' key can be of the form 'mySubModel.mySubSubModel.myField'.
-    ];
-
     /**
      * @param DatagridMapper $datagridMapper
      */
@@ -25,7 +17,16 @@ class CallBackAdmin extends Admin
     {
         $datagridMapper
             ->add('phone', null, array('label' => 'Номер телефона'))
-            ->add('status', null, array('label' => 'Статус'))
+            ->add('status', 'doctrine_orm_choice', array('label' => 'Статус'),
+                'choice',
+                array(
+                    'choices' => array(
+                        '0' => 'Ожидает',
+                        '1' => 'Принят',
+                        '2' => 'Отклонен'
+                    )
+                )
+            )
         ;
     }
 
@@ -36,7 +37,14 @@ class CallBackAdmin extends Admin
     {
         $listMapper
             ->add('phone', null, array('label' => 'Номер телефона'))
-            ->add('status', null, array('label' => 'Статус'))
+            ->add('status', 'choice', array(
+                'label' => 'Статус',
+                'choices' => array(
+                    '0' => 'Ожидает',
+                    '1' => 'Принят',
+                    '2' => 'Отклонен'
+                )
+            ))
             ->add('createTime', null, array('label' => 'Время получения'))
             ->add('updateTime', null, array('label' => 'Время последнего обновления'))
             ->add('_action', 'actions', array(
