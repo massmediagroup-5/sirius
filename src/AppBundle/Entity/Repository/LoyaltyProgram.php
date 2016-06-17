112f7bd007c9db75a603fd7a4a11275813ddf7c1
Zimm <Romsssa@gmail.com>
2016-06-17 11:44:05 +0300

    Add loyalty program

---
+++ src/AppBundle/Entity/Repository/LoyaltyProgram.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Entity\Repository;
+
+
+/**
+ * Class LoyaltyProgram
+ * @package AppBundle\Entity\Repository
+ */
+class LoyaltyProgram extends \Doctrine\ORM\EntityRepository
+{
+    /**
+     * @param $sum
+     * @return \Doctrine\ORM\Query
+     */
+    public function firstBySum($sum)
+    {
+        return $this->createQueryBuilder('loyalty')
+            ->where('loyalty.sumFrom <= :sum AND loyalty.sumTo >= :sum')
+            ->setParameter('sum', $sum)
+            ->getQuery()
+            ->getSingleResult();
+    }
+}
