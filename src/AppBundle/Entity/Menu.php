0fd6f9dee87b0aadb1b2a4cb227278426b26327b
Zimm <Romsssa@gmail.com>
2016-07-13 15:49:24 +0300

    Admin: fix model selecting in share

--- src/AppBundle/Entity/Menu.php
+++ src/AppBundle/Entity/Menu.php
@@ -155,4 +155,12 @@ class Menu
     {
         return $this->items;
     }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getName();
+    }
 }
