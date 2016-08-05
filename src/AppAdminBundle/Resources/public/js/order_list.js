4793b01dafc44f0740206d18120ae8d8c17b60b6
Zimm <Romsssa@gmail.com>
2016-08-05 14:44:12 +0300

    Add other orders to admin order page

--- src/AppAdminBundle/Resources/public/js/order_list.js
+++ src/AppAdminBundle/Resources/public/js/order_list.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).on('ready ajax.insert_content', function () {
     $('.user-orders .order-num').on('click', function (e) {
         e.preventDefault();
 
