838efd5f6c2be1496c99a674cc7116676a9df33b
Zimm <Romsssa@gmail.com>
2016-08-31 16:47:44 +0300

    Fix product codels creating

---
+++ app/DoctrineMigrations/Version20160831164511.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160831164511 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE product_models CHANGE text_label text_label VARCHAR(255) DEFAULT NULL, CHANGE text_label_color text_label_color VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE product_models CHANGE text_label text_label VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE text_label_color text_label_color VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
+    }
+}
