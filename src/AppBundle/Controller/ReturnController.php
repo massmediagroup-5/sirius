144183bd2e6b835c003566cba8555deecf75ded2
anget <amige1@mail.ru>
2016-10-24 17:18:10 +0300

    add form on return-module

--- src/AppBundle/Controller/ReturnController.php
+++ src/AppBundle/Controller/ReturnController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 class ReturnController extends Controller
 {
     /**
-     * @Route("/product/return/", name="product_return")
+     * @Route("/page/return/", name="product_return")
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
