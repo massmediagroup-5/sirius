6b4d3c25f6dda7472be88037903ffc19f620c4e2
anget <amige1@mail.ru>
2016-12-05 16:35:16 +0200

    fix

--- src/AppAdminBundle/Controller/OrderController.php
+++ src/AppAdminBundle/Controller/OrderController.php
@@ -427,6 +427,8 @@ class OrderController extends BaseController
 
         $models = $this->admin->paginateModels($filters);
 
+//        dump($models);
+
         $categories = $this->getDoctrine()->getRepository('AppBundle:Categories')->findAll();
 
         return $this->renderJson([
