4de58ae55782eef4725790b71344ca2111e314d0
Zimm <Romsssa@gmail.com>
2016-12-22 14:25:20 +0200

    Fix migration

--- app/DoctrineMigrations/Version20161221142651.php
+++ app/DoctrineMigrations/Version20161221142651.php
@@ -20,7 +20,6 @@ class Version20161221142651 extends AbstractMigration
 
         $this->addSql('ALTER TABLE loyalty_program ADD discr VARCHAR(255) NOT NULL');
         $this->addSql('UPDATE loyalty_program SET discr="simple"');
-        $this->addSql('ALTER TABLE share DROP multiple_flag, CHANGE create_time create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, CHANGE start_time start_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, CHANGE end_time end_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP');
 
         $this->addSql('INSERT INTO `loyalty_program` (`sum_from`,`sum_to`,`discount`,`discr`) VALUES (10000,15000,1,\'wholesaler\')');
         $this->addSql('INSERT INTO `loyalty_program` (`sum_from`,`sum_to`,`discount`,`discr`) VALUES (15001,20000,3,\'wholesaler\')');
@@ -37,6 +36,5 @@ class Version20161221142651 extends AbstractMigration
 
         $this->addSql('DELETE FROM loyalty_program WHERE discr = \'wholesaler\'');
         $this->addSql('ALTER TABLE loyalty_program DROP discr');
-        $this->addSql('ALTER TABLE share ADD multiple_flag TINYINT(1) NOT NULL, CHANGE create_time create_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CHANGE start_time start_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CHANGE end_time end_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
     }
 }
