cbe09753d393213b64a9030a6746e7fb1e825938
Zimm <Romsssa@gmail.com>
2017-03-30 10:45:33 +0300

    Add security rules for import

--- src/AppAdminBundle/Block/ImportBlockService.php
+++ src/AppAdminBundle/Block/ImportBlockService.php
@@ -3,6 +3,7 @@
 namespace AppAdminBundle\Block;
 
 use Sonata\BlockBundle\Block\BlockContextInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 use Sonata\AdminBundle\Form\FormMapper;
@@ -10,9 +11,21 @@ use Sonata\AdminBundle\Validator\ErrorElement;
 
 use Sonata\BlockBundle\Model\BlockInterface;
 use Sonata\BlockBundle\Block\BaseBlockService;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
 class ImportBlockService extends BaseBlockService
 {
+    /**
+     * @var AuthorizationChecker
+     */
+    protected $authorizationChecker;
+
+    public function __construct($name, EngineInterface $templating, AuthorizationChecker $authorizationChecker)
+    {
+        parent::__construct($name, $templating);
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
     public function getName()
     {
         return 'Import';
@@ -20,7 +33,7 @@ class ImportBlockService extends BaseBlockService
 
     public function getDefaultSettings()
     {
-        return array();
+        return [];
     }
 
     public function validateBlock(ErrorElement $errorElement, BlockInterface $block)
@@ -33,12 +46,17 @@ class ImportBlockService extends BaseBlockService
 
     public function execute(BlockContextInterface $block, Response $response = null)
     {
-        // merge settings
-        $settings = array_merge($this->getDefaultSettings(), $block->getSettings());
+        if ($this->authorizationChecker->isGranted('ROLE_SONATA_IMPORT')
+            || $this->authorizationChecker->isGranted('ROLE_SUPER_ADMIN')) {
 
-        return $this->renderResponse('AppAdminBundle:block:import.html.twig', array(
-            'block'     => $block->getBlock(),
-            'settings'  => $settings
-        ), $response);
+            // merge settings
+            $settings = array_merge($this->getDefaultSettings(), $block->getSettings());
+
+            return $this->renderResponse('AppAdminBundle:block:import.html.twig', array(
+                'block' => $block->getBlock(),
+                'settings' => $settings
+            ), $response);
+        }
+        return new Response();
     }
 }
